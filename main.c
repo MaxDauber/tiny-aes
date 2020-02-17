5378359fb4ec9f659ed7345febd892ee37b28f55
Max Dauber <max@dauberfamily.com>
2020-02-17 13:02:11 +0100

    added boxes from wikipedia, AES struct, reading in input files

--- main.c
+++ main.c
@@ -2,10 +2,21 @@
 #define AES_MAIN
 
 #include "aes.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	encrypt();
+int main(int argc, char *argv[]) {
+	FILE * stdin = fopen(argv[1], "r");
+
+	unsigned char key_block[4][4];
+	fread(key_block, 4, 4, stdin);
+	while(!feof(stdin)) {
+		fread(key_block, 4, 4, stdin);
+		if(!feof(stdin)) {
+			printf("%s\n", &key_block);
+		}
+	}
+	return 0;
 }
 
 #endif
