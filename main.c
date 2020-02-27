260c66ffc3b0f0d7afc1a24347f060abe902cdcc
Max Dauber <max@dauberfamily.com>
2020-02-27 01:13:21 +0100

    implemented key scheduling and round key adding

--- main.c
+++ main.c
@@ -4,27 +4,28 @@
 #include "aes.h"
 #include "stdio.h"
 #include <stdlib.h>
+#include <assert.h>
 
 int main(int argc, char *argv[]) {
-    FILE *fptr = fopen("aes_sample.in","rb");
 
     //create struct and read key
     struct AES aes_struct;
-    fread(aes_struct.key, 4, 4, fptr);
-    fread(aes_struct.state, 4, 4, fptr);
-    aes_encrypt(&aes_struct);
-    fwrite(aes_struct.state, 4, 4, stdout);
-//    //read in next block (guaranteed n < 10^6)
-//    while(!feof(stdin)) {
-//        fread(aes_struct.state, 4, 4, fptr);
-//
-//        //encrypt block and output to stdout
-//        if(!feof(stdin)) {
-//            aes_encrypt(&aes_struct);
-//            fwrite(aes_struct.state, 4, 4, stdout);
-//        }
-//    }
+
+    //read in key
+    fread(aes_struct.key, 4, 4, stdin);
+
+    //read in next block (guaranteed n < 10^6)
+    while(!feof(stdin)) {
+        fread(aes_struct.state, 4, 4, stdin);
+
+        //encrypt block and output to stdout
+        if(!feof(stdin)) {
+            aes_encrypt(&aes_struct);
+            fwrite(aes_struct.state, 4, 4, stdout);
+        }
+    }
     return 0;
+
 }
 
 #endif
